6fa87bfc48590ebeb4dcac756a47401a585ee848
canbax <yusufsaidcanbaz@gmail.com>
2020-09-20 17:54:04 +0300

    refactor/comment codes, add index.html file

--- server.js
+++ server.js
@@ -5,46 +5,77 @@ const static_data = require('./static_data.js');
 
 const baseUrl = 'http://namazvakitleri.diyanet.gov.tr/tr-TR/';
 
-app.use(function (req, res, next) {
+/** use this function like `app.use(allowOrigion4All);` for an express app
+ * Make API accessiable for all clients. Not for only clients from a specific domain.
+ */
+function allowOrigin4All(_, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
-});
+};
 
-app.get('/sehirler', (req, res) => {
-  const url = baseUrl + '/home/GetRegList?ChangeType=country&CountryId=' + req.query.ulke;
-  got(url).then(response => {
-    res.send(JSON.parse(response.body).StateList);
-  }).catch(error => {
-    console.log(error);
-    res.send(error);
-  });
-});
+/** get a list of countries
+ * @param  {} _
+ * @param  {} res
+ */
+const getCountryList = (_, res) => {
+  res.send(static_data.COUNTRIES);
+};
 
-app.get('/ilceler', (req, res) => {
-  const url = baseUrl + `/home/GetRegList?ChangeType=state&CountryId=${req.query.ulke}&StateId=${req.query.sehir}`;
+/** get a list of cities/states for given country.
+ * @param  {} req
+ * @param  {} res
+ */
+const getCityList = (req, res) => {
+  const id = req.query.country;
+  if (id == 2) {
+    res.send(static_data.TR_CITIES);
+  } else {
+    const url = baseUrl + '/home/GetRegList?ChangeType=country&CountryId=' + id;
+    got(url).then(response => {
+      res.send(JSON.parse(response.body).StateList);
+    }).catch(error => {
+      console.log(error);
+      res.send(error);
+    });
+  }
+};
+
+/** get a list of regions/districts for given city/state.
+ * @param  {} req
+ * @param  {} res
+ */
+const getRegionsList = (req, res) => {
+  const country = req.query.country;
+  const city = req.query.city;
+  const url = baseUrl + `/home/GetRegList?ChangeType=state&CountryId=${country}&StateId=${city}`;
   got(url).then(response => {
     res.send(JSON.parse(response.body).StateRegionList);
   }).catch(error => {
     console.log(error);
     res.send(error);
   });
-});
+};
 
-app.get('/vakitler', (req, res) => {
-  const url = baseUrl + req.query.ilce;
+/** get times data for a region/district, returns an string[][].
+ * @param  {} req
+ * @param  {} res
+ */
+const getTimeData4Region = (req, res) => {
+  const url = baseUrl + req.query.region;
   got(url).then(response => {
-
     const t = findTableWithMonthInfo(response.body);
     const d = getDataFromTable(t);
-
     res.send(d);
   }).catch(error => {
     console.log(error);
     res.send(error);
   });
-});
+};
 
+/**  returns values inside <td> as string[][]
+ * @param  {string} str HTML table as string
+ */
 function getDataFromTable(str) {
   const arr = [];
   str = str.replace(/<\/tr>/g, '');
@@ -63,6 +94,9 @@ function getDataFromTable(str) {
   return arr;
 }
 
+/** find a table that contains 28-31 rows to find monthly data
+ * @param  {} str webpage as HTML string
+ */
 function findTableWithMonthInfo(str) {
   let idx = 0;
   while (true) {
@@ -79,11 +113,16 @@ function findTableWithMonthInfo(str) {
   }
 }
 
-
+/** count number of rows in table
+ * @param  {} table HTML table as string
+ */
 function countRowsInTable(table) {
   return (table.match(/<tr>/g) || []).length;
 }
 
+/** find the substring for table inside 'str' 
+ * @param  {} str webpage as HTML string
+ */
 function findTable(str) {
   const t1 = str.indexOf('<tbody');
   const t2 = str.indexOf('/tbody>');
@@ -92,6 +131,14 @@ function findTable(str) {
   }
   return { str: str.substring(t1, t2), lastIdx: t2 + 7 };
 }
+
+app.use(allowOrigin4All);
+app.use(express.static('public'));
+console.log('__dirname: ', __dirname);
+app.get('/countries', getCountryList);
+app.get('/cities', getCityList);
+app.get('/regions', getRegionsList);
+app.get('/data', getTimeData4Region);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log('namaz vakti listening on 3000'));
