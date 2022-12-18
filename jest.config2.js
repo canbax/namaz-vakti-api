c92a1eba312b04d52c0fe1eaf270031e05e8ee43
canbax <yusufsaidcanbaz@gmail.com>
2022-12-18 12:32:23 +0300

    - new jest config for detailed coverage report - lighter pre-commit

---
+++ jest.config2.js
@@ -0,0 +1,7 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  coveragePathIgnorePatterns: ["/node_modules/", "/data/", "/coverage/"],
+  coverageReporters: ["html"],
+};
